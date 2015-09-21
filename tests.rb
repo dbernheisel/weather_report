d02efe84adcdd6a15bd3fb52c20de446d37ec9a0
David Bernheisel <david@bernheisel.com>
2015-09-21 17:38:36 -0400

    change .api to _response

--- tests.rb
+++ tests.rb
@@ -25,17 +25,19 @@ sample_hourly_10day_forecast_response = nil
 
 File.open('tests.json') do |f|
   all_responses = JSON.parse(f)
-  sample_astronomy_response = all_responses["astronomy.api"]
-  sample_current_alerts_response = all_responses["current_alerts.api"]
-  sample_current_conditions_response = all_responses["current_conditions.api"]
-  sample_current_hurricanes_response = all_responses["current_hurricanes.api"]
-  sample_daily_forecast_response = all_responses["daily_forecast.api"]
-  sample_daily_10day_forecast_response = all_responses["daily_10day_forecast.api"]
-  sample_hourly_forecast_response = all_responses["hourly_forecast.api"]
-  sample_hourly_10day_forecast_response = all_responses["hourly_10day_forecast.api"]
+  sample_astronomy_response = all_responses["astronomy_response"]
+  sample_current_alerts_response = all_responses["current_alerts_response"]
+  sample_current_conditions_response = all_responses["current_conditions_response"]
+  sample_current_hurricanes_response = all_responses["current_hurricanes_response"]
+  sample_daily_forecast_response = all_responses["daily_forecast_response"]
+  sample_daily_10day_forecast_response = all_responses["daily_10day_forecast_response"]
+  sample_hourly_forecast_response = all_responses["hourly_forecast_response"]
+  sample_hourly_10day_forecast_response = all_responses["hourly_10day_forecast_response"]
   all_responses = nil
 end
 
 class WundergroundTests < ActiveSupport::TestCase
+  def test_astronomy
 
+  end
 end
